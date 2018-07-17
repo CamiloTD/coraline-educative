52ddb513b9a644c0922ada14a7b9c02316df8093
CamiloTD <camilotd1999@gmail.com>
2018-07-17 11:34:49 -0500

    i18n Support added

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
+const pug = require('gulp-pug-i18n');
 const watch = require('gulp-watch');
 const stylus = require('gulp-stylus');
 
 const DEBUG = true;
 
-function watchPug(cb, ignoreInitial) {
-    'use strict';
+function watchPug(cb, ignoreInitial = false) {
     let pug_obj = pug({
+        i18n: {
+            locales: 'lang/*.*',
+            dest: 'public',
+            namespace: 'Lang',
+            filename: '{{lang}}/{{basename}}.html'
+        },
         basedir: 'src',
         doctype: 'html',
         pretty: DEBUG,
-        self: true,
         cache: false
     }).on('error', function (error) {
         console.log(error);
         watchPug(cb, true);
     });
 
-    return watch('src/**/*.pug', {ignoreInitial: ignoreInitial}).pipe(pug_obj).pipe(gulp.dest('dist'));
+    return watch('src/**/*.pug', {ignoreInitial: ignoreInitial}).pipe(pug_obj).pipe(gulp.dest('public'));
 
 }
 
@@ -31,7 +35,7 @@ function watchStylus(cb, ignoreInitial = false) {
         watchStylus(cb, true);
     });
 
-    return watch('src/**/*.styl', {ignoreInitial: ignoreInitial}).pipe(styl_obj).pipe(gulp.dest('dist'));
+    return watch('src/**/*.styl', {ignoreInitial: ignoreInitial}).pipe(styl_obj).pipe(gulp.dest('public'));
 }
 
 gulp.task('watch-stylus', watchStylus);
