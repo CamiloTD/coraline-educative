2ab8460a33f547c8dfff1143f5a3695df90a7429
CamiloTD <camilotd1999@gmail.com>
2018-08-10 12:29:17 -0500

    Added category count

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 let $ = require('jquery');
 let Lang = require('/lang');
 let Vue = require('vue');
+let io = require('/socket.io/socket.io.js');
+let info = io('/info');
+let Categories = require('/data/categories.json');
 let Utils = require('./utils');
 			require('/css/base.css');
 			require('/components/intro')();
@@ -11,7 +14,8 @@ let app = new Vue({
 	data: {
 		Lang: Lang(),
 		Categories: require('/data/categories.json'),
-		coralines: {}
+		coralines: {},
+		package_count: {}
 	},
 
 	methods: {
@@ -19,6 +23,23 @@ let app = new Vue({
 		scrollTop: () => Utils.scrollTo(0)
 	}
 });
+
+
+info.on('packages', (data) => {
+	let package_count = {};
+	
+	Categories.forEach((c) => package_count[c] = 0);
+
+	for(let i in data) {
+		let cora = data[i];
+
+		cora.categories.forEach((cat) => package_count[cat]++);
+	}
+
+	app.package_count = package_count;
+});
+
+info.emit('packages');
 
 require('semantic-ui');
 require('/css/index.css');
